7100ad8b489dd2899a23dff92130aeff52d05933
hafid <hadjadj.abdelhafidh@gmail.com>
2022-07-01 00:08:10 +0100

    v7.1

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-// export const API_URL = "https://modern-house-agency.herokuapp.com";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://modern-house-agency.herokuapp.com";
